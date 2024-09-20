8b63d74280ed26c89ea6f6b5ec9b3db061649c19
Misas10 <manmisa2003@gmail.com>
2024-09-20 15:25:02 +0100

    Compile_commands.json added

--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
 
 #include "definitons.h"
-#include "lifecycle.h"
-#include "render.h"
-#include "load.h"
 #include "events.h"
-#include <stdio.h>
+#include "lifecycle.h"
+#include "load.h"
+#include "render.h"
 
 int main() {
   Game.init();
@@ -14,30 +13,29 @@ int main() {
 
   SDL_Event event;
 
-  while(Game.isRunning) {
+  while (Game.isRunning) {
 
-    if(SDL_WaitEvent(&event)){
-      switch (event.type) { 
-        case SDL_QUIT:
+    if (SDL_WaitEvent(&event)) {
+      switch (event.type) {
+      case SDL_QUIT:
+        Game.isRunning = false;
+        break;
+
+      case SDL_KEYDOWN:
+        if (event.key.keysym.sym == SDLK_q) {
+          printf("'q' Key pressed, exiting the game\n");
           Game.isRunning = false;
-          break;
+        }
 
-        case SDL_KEYDOWN:
-          if(event.key.keysym.sym  == SDLK_q) {
-            printf("'q' Key pressed, exiting the game\n");
-            Game.isRunning = false;
-          }
+        if (event.key.keysym.sym == SDLK_i)
+          gameData.isReversed = !gameData.isReversed;
 
-          if(event.key.keysym.sym  == SDLK_i)
-            gameData.isReversed = !gameData.isReversed;
-
-          break;
+        break;
       }
     }
 
     render();
     /*SDL_*/
-
   }
   Game.quit();
 
