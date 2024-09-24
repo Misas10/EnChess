47b2fc5372c6ed78c04c2f3a47bb8e2ee89bc934
Misas10 <manmisa2003@gmail.com>
2024-09-24 12:55:19 +0100

    Added bitboard

--- include/lifecycle.h
+++ include/lifecycle.h
@@ -1,8 +1,8 @@
 #ifndef LIFECYCLE_H
 #define LIFECYCLE_H
 
+#include "SDL2/SDL2/SDL_stdinc.h"
 #include "definitons.h"
-#include <SDL2/SDL_stdinc.h>
 
 Board boardInfo = {
     {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
@@ -14,6 +14,57 @@ Board boardInfo = {
     {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
     {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
 };
+
+Uint64 bitboard = 0ULL;
+
+void add_in_position(int bit_pos) { bitboard |= (1ULL << bit_pos); }
+
+Uint64 get_mask(int bit_pos, Uint64 offset) { return ~(offset << bit_pos); };
+
+void remove_piece(int bit_pos) { bitboard &= get_mask(bit_pos, 1ULL); };
+
+typedef enum {
+  east = 1,
+  west = -1,
+  north = -8,
+  south = 8,
+  nortEast = -7,
+  nortWest = -9,
+  southWest = 7,
+  southEast = 9
+} Coord;
+
+void piece_shift(int bit_pos, Coord coord) {
+  // Clear the bit
+  remove_piece(bit_pos);
+
+  if (gameData.isReversed)
+    coord *= -1;
+
+  // Add the bit in the new position
+  add_in_position(bit_pos + coord);
+}
+
+void print_board() {
+  printf("   ");
+  for (int i = 'a'; i < 'a' + 8; i++)
+    printf(" %c", i);
+
+  printf("\n");
+
+  for (int rank = 0; rank < 8; rank++) {
+    for (int file = 0; file < 8; file++) {
+      // Calculate square with the coordinates
+      int square = rank * 8 + file;
+
+      if (!file)
+        printf(" %d ", 8 - rank);
+
+      printf(" %d", (bitboard & (1ULL << square)) ? 1 : 0);
+    }
+    printf("\n");
+  }
+}
 
 GameInstance Game = {
     SDL_FALSE,
@@ -27,6 +78,11 @@ GameData gameData = {PLAYER, {-1, -1}, SDL_FALSE};
 void newGame();
 
 void init(void) {
+
+  add_in_position(e3);
+  piece_shift(e3, north);
+
+  print_board();
 
   if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
     SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "SDL Error", SDL_GetError(),
@@ -60,7 +116,7 @@ void init(void) {
     return;
   }
 
-  Game.isRunning = SDL_TRUE;
+  // Game.isRunning = SDL_TRUE;
 
   /*memmove(Game.data.board, boardData, sizeof(Board));*/
 };
